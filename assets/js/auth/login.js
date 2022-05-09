9ce7f04da19aa3edd873be13d155c6ba0f743a5a
HyukHyunCho <gurgus0189@gmail.com>
2022-05-09 17:26:14 +0900

    login add

---
+++ assets/js/auth/login.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+    $("#loginBtn").click(function() {
+        location.href = "../pages/dashboard.html";
+    });
+
+    $("#signUpBtn").click(function() {
+        location.href = "../pages/signUp.html"; 
+    });
+    
+
+
+
+});
--- assets/js/machineManagement/machineManagement.js
+++ assets/js/machineManagement/machineManagement.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+    aaa();
+
     $("#detailPageBtn").click(function() {
 		location.href = "machineManagementDetail.html";
 	}); 
@@ -20,10 +22,6 @@ $(document).ready(function() {
 		location.href = "machineManagementDetail.html";
 	});
 
-
-
-
-
     $("#addModalBtn").click(function() {
         $("#modal").css("display","block");
     });
@@ -43,3 +41,33 @@ $(document).ready(function() {
     
 });
 
+
+function aaa() {
+
+	var url = "http://192.168.0.8:7579/VITCON-Dev";
+	
+	$.ajax({
+        headers: {
+            'Accept': 'application/json',
+            'X-M2M-RI': 1,
+            'X-M2M-Origin': 'Cmyself',
+            'X-M2M-RVI': '2a',
+            'X-M2M-TY': 4
+        },
+		url : url,
+		type : 'get',
+		//data : parameter,
+		success : function(response) {
+			
+            
+			console.log(response["m2m:cb"]);
+
+            
+
+		},
+		error : function () {
+			alert("error");
+		}  
+	});
+	
+}
